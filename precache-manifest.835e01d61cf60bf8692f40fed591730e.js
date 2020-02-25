5a3c19822e84ea11e873f1edc3d2b75e842f73ff
Travis Tang <travis.tang@tum.de>
2020-02-25 14:36:33 +0100

    Updates

---
+++ precache-manifest.835e01d61cf60bf8692f40fed591730e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d98d33c2c114ce50199b77965d373f",
+    "url": "/Inventory-v2/index.html"
+  },
+  {
+    "revision": "7107817019b0835fad9a",
+    "url": "/Inventory-v2/static/css/2.a6ab3c2a.chunk.css"
+  },
+  {
+    "revision": "3ed8f6f5548fe21fb009",
+    "url": "/Inventory-v2/static/css/main.27fb1efc.chunk.css"
+  },
+  {
+    "revision": "7107817019b0835fad9a",
+    "url": "/Inventory-v2/static/js/2.89488ee4.chunk.js"
+  },
+  {
+    "revision": "6bfd20a268a15ede7e42c7b94779be5d",
+    "url": "/Inventory-v2/static/js/2.89488ee4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ed8f6f5548fe21fb009",
+    "url": "/Inventory-v2/static/js/main.858620c2.chunk.js"
+  },
+  {
+    "revision": "a32985cf4bc1485289aa",
+    "url": "/Inventory-v2/static/js/runtime-main.a026317a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Inventory-v2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Inventory-v2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Inventory-v2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Inventory-v2/static/media/slick.f97e3bbf.svg"
+  }
+]);
